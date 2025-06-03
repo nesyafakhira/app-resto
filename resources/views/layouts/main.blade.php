f36c9ff627ea568df73890c237d6dcc6af549b20
nesyafakhira <nesyafkhrarianti75@gmail.com>
2025-06-03 12:29:15 +0700

    add template

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <a class="navbar-brand col-md-3 col-lg-2" href="#">
+          <img src="{{ asset('images/logo-indi.png')}}" width="60" alt="Logo Indi">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <form action="{{ route('logout') }}" method="post" id="logout">
+                @csrf
+                <a onclick='document.getElementById("logout").submit()' href="#" class="nav-link px-3 text-dark logout">Logout<i class="bi bi-box-arrow-right"></i></a>
+             </form>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+      <div class="container mt-4">
+        @yield('container')
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  </body>
+</html>
