26f1043bb008ed387cfd5c5cb59e3b31c299ed09
nesyafakhira <nesyafkhrarianti75@gmail.com>
2025-06-07 15:05:13 +0700

    add dashboard template

---
+++ resources/views/dashboard/layouts/table.blade.php
@@ -0,0 +1,213 @@
+<div class="col-md-12 col-lg-12">
+    <div class="card overflow-hidden">
+        <div class="card-header d-flex justify-content-between flex-wrap">
+            <h5>Transaction History</h5>
+            <div class="dropdown">
+                <span class="dropdown-toggle" id="dropdownMenuButton7" data-bs-toggle="dropdown" aria-expanded="false" role="button">
+                </span>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton7">
+                    <a class="dropdown-item " href="javascript:void(0);">Action</a>
+                    <a class="dropdown-item " href="javascript:void(0);">Another action</a>
+                    <a class="dropdown-item " href="javascript:void(0);">Something else here</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body p-0">
+            <div class="table-responsive mt-4">
+                <table id="basic-table" class="table table-striped table-hover" role="grid">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Transaction ID</th>
+                            <th>Date and Time</th>
+                            <th class="text-end">Amount</th>
+                            <th class="text-center">Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 1</p>
+                                </div>
+                            </td>
+                            <td>
+                                Oct 16, 2021, 10.45am
+                            </td>
+                            <td class="text-end">$10.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-success">
+                                    Completed
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 2</p>
+                                </div>
+                            </td>
+                            <td>
+                                Mar 01, 2021, 5.30pm
+                            </td>
+                            <td class="text-end">$20.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-warning">
+                                    Pending
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 3</p>
+                                </div>
+                            </td>
+                            <td>
+                                Apr 05, 2021, 1:40pm
+                            </td>
+                            <td class="text-end">$30.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-info">
+                                    For Pickup
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 4</p>
+                                </div>
+                            </td>
+                            <td>
+                                Dec 20, 2021, 4:30pm
+                            </td>
+                            <td class="text-end">$40.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-danger">
+                                    Declined
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 5</p>
+                                </div>
+                            </td>
+                            <td>
+                                Dec 20, 2021, 4.30pm
+                            </td>
+                            <td class="text-end">$40.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-warning">
+                                    Pending
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="">
+                                    <h6 class="text-primary">#12345678</h6>
+                                    <p class="mb-0">Payment Number 6</p>
+                                </div>
+                            </td>
+                            <td>
+                                Oct 16, 2021, 10.45am
+                            </td>
+                            <td class="text-end">$40.00</td>
+                            <td class="text-center">
+                                <div class="badge rounded-pill bg-soft-success">
+                                    Completed
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div class="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                        <li><a class="dropdown-item" href="#">This Week</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
